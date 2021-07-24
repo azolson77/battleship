c348ce6327b7d130a8ed9789f233efdbf8396df1
azolson77 <87374178+azolson77@users.noreply.github.com>
2021-07-24 13:16:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
+# This application was made to implement a simple one player game of battleship
+# Written by Alex Olson
+
 from flask import Flask  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
-    """Say hello"""
-    return 'Hello, world!'
+
+    import random
+    import numpy as np
+
+    # Width and Height of the board
+    WIDTH, HEIGHT = 10, 10
+
+    # Variable containing the game board
+    board = np.zeros((WIDTH, HEIGHT))
+
+    # Coordinates of battleship on the game board
+    battleship_x = random.randint(0, 10)
+    battleship_y = random.randint(0, 10)
+    coords = (battleship_x, battleship_y)
+
+    # Boolean checking for victory
+    win = False
+
+    # Counter for number of turns
+    turn = 0
+    count = 10
+
+    print("Battleship located at: " + str(coords))
+
+    # Main game loop
+    while not win:
+        print(board)
+        print("Turns left: " + str((count - turn)))
+
+        # Check if turns left
+        if turn == count:
+            print("Out of ammo, you lose!")
+            quit()
+
+        # Get player input for x and y coords
+        x = int(input('Select x coordinate: '))
+        y = int(input('Select y coordinate: '))
+        aim = (x, y)
+        print("You shoot at " + str(aim))
+
+        if coords == (x, y):
+            print("You sunk the battleship!")
+            win = True
+            quit()
+        else:
+            print("Miss!")
+            board[x][y] = -1
+            turn += 1
+            print(board)
+
 
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
